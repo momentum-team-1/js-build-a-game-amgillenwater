876cc7cf2b0762122e125aac12fcb9eba40a082f
amgillenwater <amgillenwater@gmail.com>
2020-05-12 18:39:26 -0400

    stop adding functions inside constructor functions yikes

--- main.js
+++ main.js
@@ -18,114 +18,45 @@ class Game {
         context.fillRect(50, 50, 400, 400);
         let gameSize = { x: canvas.width, y: canvas.height }
         this.player = new Player(gameSize)
+        let animate = () => {
+            this.update()
+            this.drawPlayer(context, gameSize)
+            requestAnimationFrame(animate)
+        }
+        animate()
     }
         drawPlayer (context, gameSize) {
             console.log("draw player method called")
             context.fillStyle= '#79C99E'
-            
+            let startingX = this.player.center.x - this.player.size.x / 2
+            let startingY = this.player.center.y - this.player.size.y / 2
+            let playerWidth = this.player.size.x
+            let playerHeight = this.player.size.y
             context.fillRect(startingX, startingY, playerWidth, playerHeight)
+            context.clearRect (0, 0, gameSize.x, gameSize.y) ;
         }
-        // //create the array to hold all of the pieces
-        // this.bodies = []
-        // let tick = () => {
-        //     // Update game state.
-        //       this.update()
-        
-        //       // Draw game bodies.
-        //       this.draw(context, gameSize)
-        
-        //       // Queue up the next call to tick with the browser.
-        //       requestAnimationFrame(tick)
-        //     }
-        //     tick()
-        // //add enemies--why do we pass through this class?
-        // this.bodies = this.bodies.concat(createEnemies(this))
-        // this.bodies = this.bodies.concat(new Player(this, gameSize))
-        
-}
+
+        update (){
+            this.player.update();
+        }
+    }
+
 
 class Player{
     constructor (gameSize){
-        this.size = { x: 30, y: 30 } 
-        this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2}
+        this.size = { x: 30, y: 30 };
+        this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2};
+        this.keyboarder = Keyboarder
     }
+    update() {
+        if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+            console.log("RIGHT ARROW WAS PRESSED")
+            this.center.x += 2
+        } else if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)){
+            console.log("LEFT ARROW WAS PRESSED")
+            this.center.x -= 2
+        }
+
 }
 
-// function update () {
-//     // `notCollidingWithAnything` returns true if passed body
-//     // is not colliding with anything.
-//     let notCollidingWithAnything = (b1) => {
-//       return this.bodies.filter(function (b2) { return colliding(b1, b2) }).length === 0
-//     }
-
-//     // Throw away bodies that are colliding with something. They
-//     // will never be updated or draw again.
-//     this.bodies = this.bodies.filter(notCollidingWithAnything)
-
-//     // Call update on every body.
-//     for (let i = 0; i < this.bodies.length; i++) {
-//       this.bodies[i].update()
-//     }
-// }
-
-// // PLAYER CLASS
-// //     RESPONSIBILITIES
-// //         MOVE USING THE ARROW KEYS (UP,DOWN,LEFT,RIGHT)
-// //     COLLABORATORS
-// //         ENEMY CLASS
-// //         COIN CLASS
-// // class Player{
-// //     constructor(){
-
-// //     }
-// //     //BEHAVIORS: MOVE USING THE ARROW KEYS + COLLIDE WITH COIN TO "TAKE"
-        
-// //     }
-// // COIN CLASS
-// //     RESPONSIBILITIES
-// //         MOVE TO A NEW SPOT WHEN THE PLAYER TOUCHES THE COIN
-// //         TRIGGER THE SCORE WHEN TOUCHED
-// //     COLLABORATORS
-// //         PLAYER SQUARE
-// //         SCORE
-
-// // class Coin{
-// //     constructor(){
-// //         this.taken = false; //this value will start as false because the coin has not been "taken" by the player yet
-// //     }
-// // }
-// // START ON A RANDOM SPOT ON BOARD
-// // TRIGGER POINT WHEN COLLIDE WITH PLAYER
-// // ADD POINT TO SCORE
-// // RESET IN A NEW, RANDOM SPOT 
-
-
-// // ENEMIES CLASS
-// //     RESPONSIBILITIES
-// //         MOVE FROM L TO R, R TO L, TOP TO BOTTOM, OR BOTTOM TO TOP IN 3 DISTINCT PATHS IN EACH DIRECTION -- BEHAVIORS
-// //         AFTER COLLISION WITH PLAYER, RESET BOARD AND SCORE TO ZERO
-// //         WILL BE MULTIPLE ONES MOVING AT A TIME--ADD IDS?
-// //     COLLABORATORS
-// //         PLAYERS
-        
-        
-// // class Enemies{
-// //     constructor(){
-// //         this.id = `enemy-${index}`
-// //     }
-// // }
-
-
-// function createEnemies(num) {
-//     const enemies = [];
-//     //empty array, the loop will iterate num times when called to create the array for enemies
-
-//     for (let i=0; i < num; i++) {
-//         let enemy = new Enemy(i, this);
-//         enemies.push(enemy);
-//     }   
-    
-//     return enemies;
-// }
-
-
+new Game()
